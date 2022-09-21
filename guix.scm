5a03e3183f921b6328e2b6bc58fb7e1c7628f617
Christopher Rodriguez <yewscion@gmail.com>
2022-09-21 18:23:12 -0400

    Initial Commit, Using Calculator Example.

---
+++ guix.scm
@@ -0,0 +1,47 @@
+(use-modules
+ ;;; These are my commonly needed modules; remove unneeded ones.
+ (guix packages)
+ ((guix licenses) #:prefix license:)
+ (guix download)
+ (guix build-system gnu)
+ (gnu packages)
+ (gnu packages autotools)
+ (gnu packages pkg-config)
+ (gnu packages texinfo)
+ (gnu packages guile)
+ (gnu packages java)
+ (gnu packages haskell)
+ (gnu packages haskell-xyz)
+ (cdr255 programming)
+ (guix gexp))
+
+(package
+  (name "pseudotaxus")
+  (version "0.0.1")
+  (source (local-file (string-append "./"
+                                     name
+                                     "-"
+                                     version
+                                     ".tar.bz2")))
+  (build-system gnu-build-system)
+  (arguments
+   `(#:tests? #f
+     ;; #:phases
+     ;; (modify-phases
+     ;;     %standard-phases)
+     ))
+  (native-inputs (list autoconf automake pkg-config texinfo))
+  (inputs (list ghc ghc-bnfc ghc-alex ghc-happy))
+  (synopsis "Somewhat-standardized pseudocode syntax")
+  (description
+   "Pseudotaxus is a collection of standard symbols (words and punctuation) that
+lend some consistent form to the definition of an algorithm in pseudocode.
+It's meant to provide pseudocode with an interpretable form,
+syntax-highlighting, and most importantly a limit on which words carry
+predefined meaning in a listing.")
+  (home-page
+   "https://cdr255.com/projects/pseudotaxus")
+  (license license:agpl3+))
+;; Local Variables:
+;; mode: scheme
+;; End:
